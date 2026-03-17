2fcc050ac73157b04f6ad7cba86b68afdaadbe13
Lehay304 <158135576+Lehay304@users.noreply.github.com>
2026-03-17 18:05:12 +0700

    Checkpoint 1: Были сделаны миграции

---
+++ gusinin-booking-api/database/migrations/2026_03_17_105226_create_resources_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('resources', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // названия напр. 'русская баня, 'Финская сауна'
+            $table->text('description')->nullable();
+            $table->string('location'); // это адрес или номер комнаты в бане
+            $table->unsignedInteger('capacity'); // вместимость это вместимость
+            $table->json('features')->nullable(); // это всякие приблуды по типу: 'пар', 'веники', 'душ', 'температура_110']
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('resources');
+    }
+};
